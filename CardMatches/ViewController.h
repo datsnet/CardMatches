bdd95247e5a5cd8fc6560a4165c99d1f56414c95
datsnet <datsumi1013@gmail.com>
2014-04-01 23:10:12 +0900

    new

--- CardMatches/ViewController.h
+++ CardMatches/ViewController.h
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Card.h"
 
 @interface ViewController : UIViewController
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardsButton;
 @property (strong, nonatomic) NSMutableArray *cards;
+
 
 
 - (IBAction)pushCard:(UIButton *)sender;
